426d1209f413b48a7dbbae4b2490a534a2aca03d
fengzhao <fengzhao1124@hotmail.com>
2022-12-15 11:21:10 +0800

    Update gitlab_backup.sh

--- gitlab_backup.sh
+++ gitlab_backup.sh
@@ -6,15 +6,15 @@ set -u
 # 如果执行遇到错误，自动退出
 set -o errexit
 
-## gitlab备份，本地备份后可以传到远端
+## gitlab定期备份脚本，在gitlab服务器配置此脚本后，定期自动备份
+
+## gitlab备份，本地备份后可以传到远端的专用备份服务器上
 
 # 执行一次gitlab备份
 /opt/gitlab/bin/gitlab-rake gitlab:backup:create CRON=1
 
-
 # gitlab 在本地备份路径，可以在/etc/gitlab/gitlab.rb文件中配置
 LocalBackDir=/var/opt/gitlab/backups
-
 
 # 远程备份服务器的IP地址
 RemoteIP=10.10.20.199
@@ -22,11 +22,8 @@ RemoteIP=10.10.20.199
 # 远程备份服务器的登录账户
 RemoteUser=root
 
-
 # 远程备份服务器上的gitlab备份文件存放路径
 RemoteBackDir=/backup/gitlab/
-
-
 
 #当前系统日期
 DATE=`date +"%Y-%m-%d"`
@@ -48,7 +45,6 @@ echo "--------------------------------------------------------------------------
 
 # 输出日志，打印出每次scp的文件名
 echo "---------------------The file to scp to remote server is: $BACKUPFILE_SEND_TO_REMOTE-------------------------------" >> ${LogFile}
-
 
 
 #备份到远程服务器
